f858d64827bf6aaa29463c05892a013b2e6f98f4
M Sajid Ali <msajidm9909@gmail.com>
2025-02-14 20:42:12 +0500

    first commit

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import ComponentA from './Components/ComponentA'
+import ComponentB from './Components/ComponentB'
+
+function App() {
+  return (
+    <div className='flex  items-center justify-center '>
+      {/* <ComponentA/> <br /> <br /> */}
+      <ComponentB/>
+    </div>
+  )
+}
+
+export default App
---
+++ vite-project/src/Components/ComponentB.jsx
@@ -0,0 +1,38 @@
+import React, { useState } from "react";
+
+function ComponentB() {
+  const [name, setName] = useState({
+    name: "sajid",
+    age: 25,
+    address: "sgd",
+    class: "FA IT",
+    college: "govt.",
+  });
+  let changeName = () => {
+    setName({
+      name: "Mateen",
+      age: 20,
+      address: "kotmomin",
+      class: "FSC BIOLOGY",
+      college: "Superior group of colleges",
+    });
+  };
+  console.log(name);
+
+  return (
+    <>
+      <div className=" gap-4 mt-4">
+        <p  className="text-xl font-bold text-gray-700">{name.name}</p>
+        <p className="text-xl font-bold  text-gray-700">{name.age}</p>
+        <p className="text-xl font-bold  text-gray-700">{name.address}</p>
+        <p className="text-xl font-bold text-gray-700 ">{name.class}</p>
+        <p className="text-xl font-bold text-gray-700 ">{name.college}</p>
+      </div>
+      <div>
+      <button  className="text-xl font-bold  border rounded-md bg-slate-50 p-1 " onClick={changeName}>change</button>
+      </div>
+    </>
+  );
+}
+
+export default ComponentB;
